807b6654fc96b93614bc92d87af0e176c3bdac8a
Pablo Rodriguez rodriguez <pablorodriguezrodriguez@MacBook-Air-de-Pablo.local>
2024-04-16 20:18:40 +0200

    añadida la entidad inventario

---
+++ TFCPRR/src/main/java/Entidades/Inventario.java
@@ -0,0 +1,75 @@
+package Entidades;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Inventario")
+public class Inventario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private int idProducto;
+
+    @ManyToOne
+    @JoinColumn(name = "id_restaurante", referencedColumnName = "id_restaurante")
+    private Restaurante restaurante;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "cantidad")
+    private Integer cantidad;
+
+    @Column(name = "precio_unitario")
+    private Double precioUnitario;
+
+    public Inventario() {
+    }
+
+    public Inventario(Restaurante restaurante, String nombre, Integer cantidad, Double precioUnitario) {
+        this.restaurante = restaurante;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public Restaurante getRestaurante() {
+        return restaurante;
+    }
+
+    public void setRestaurante(Restaurante restaurante) {
+        this.restaurante = restaurante;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(Double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+}
