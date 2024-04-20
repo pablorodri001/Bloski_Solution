eca027f60bd139e8eb3925f6046d72d77065336a
Pablo Rodriguez rodriguez <pablorodriguezrodriguez@MacBook-Air-de-Pablo.local>
2024-04-20 18:11:44 +0200

    login funciona correctamente

--- TFCPRR/src/main/java/module-info.java
+++ TFCPRR/src/main/java/module-info.java
@@ -4,10 +4,12 @@ module com.example.tfcprr {
     requires java.persistence;
     requires org.hibernate.orm.core;
     requires java.naming;
-
+    requires transitive java.sql;
 
     opens com.example.MainPackage to javafx.fxml;
     exports com.example.MainPackage;
     exports Controllers;
     opens Controllers to javafx.fxml;
+
+    opens Entidades;
 }
