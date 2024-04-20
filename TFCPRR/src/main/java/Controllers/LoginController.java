a7172a7093c1afd1dc9c479f4303e45e737f32af
Pablo Rodriguez rodriguez <pablorodriguezrodriguez@MacBook-Air-de-Pablo.local>
2024-04-20 18:04:15 +0200

    eliminado una clase y afinando el mapeado

--- TFCPRR/src/main/java/Controllers/LoginController.java
+++ TFCPRR/src/main/java/Controllers/LoginController.java
@@ -1,12 +1,11 @@
 package Controllers;
 
+import UtilidadesEntidades.HibernateUtil;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import Entidades.Usuarios;
-import UtilidadesEntidades.UsuarioDAO;
 
 public class LoginController extends GenericController {
 
@@ -22,22 +21,22 @@ public class LoginController extends GenericController {
     @FXML
     private PasswordField txtNuevaContrasena;
 
-    private UsuarioDAO usuarioDAO = new UsuarioDAO();
+
 
     @FXML
     public void onIniciarSesionClick(ActionEvent actionEvent) {
         String nombreUsuario = txtNombreUsuario.getText();
         String contrasena = txtContrasena.getText();
 
-        Usuarios usuario = usuarioDAO.obtenerUsuarioPorNombreYContrasena(nombreUsuario, contrasena);
+        Usuarios usuario = HibernateUtil.obtenerUsuarioPorNombreYContrasena(nombreUsuario, contrasena);
 
         if (usuario != null) {
-            // Si el usuario existe, no hacemos nada
+
             System.out.println("¡Inicio de sesión exitoso!");
-            // Puedes agregar aquí cualquier otra acción que desees
+
         } else {
             System.out.println("Credenciales incorrectas");
-            // Puedes mostrar una alerta o mensaje al usuario aquí
+
         }
     }
 
@@ -48,14 +47,14 @@ public class LoginController extends GenericController {
 
         Usuarios nuevoUsuario = new Usuarios(nuevoNombreUsuario, nuevaContrasena);
 
-        boolean registroExitoso = usuarioDAO.insertarUsuario(nuevoUsuario);
+        boolean registroExitoso = HibernateUtil.insertarUsuario(nuevoUsuario);
 
         if (registroExitoso) {
             System.out.println("¡Registro exitoso!");
-            // Puedes agregar aquí cualquier otra acción que desees
+
         } else {
             System.out.println("Error al registrar usuario");
-            // Puedes mostrar una alerta o mensaje al usuario aquí
+
         }
     }
 }
--- TFCPRR/src/main/java/UtilidadesEntidades/HibernateUtil.java
+++ TFCPRR/src/main/java/UtilidadesEntidades/HibernateUtil.java
@@ -1,5 +1,7 @@
 package UtilidadesEntidades;
 
+import Entidades.Usuarios;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
@@ -7,28 +9,31 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateUtil {
-    private static final SessionFactory sessionFactory;
+    static  StandardServiceRegistry sr = new StandardServiceRegistryBuilder().configure().build();
+    static  SessionFactory sf = new MetadataSources(sr).buildMetadata().buildSessionFactory();
+    public static Usuarios obtenerUsuarioPorNombreYContrasena(String nombreUsuario, String contrasena) {
+        Session sesion=sf.openSession();
+        sesion.beginTransaction();
+        return sesion.createQuery("FROM Usuarios WHERE nombreUsuario = :nombreUsuario AND contrasena = :contrasena", Usuarios.class)
+                .setParameter("nombreUsuario", nombreUsuario)
+                .setParameter("contrasena", contrasena)
+                .uniqueResult();
 
-    static {
-        try {
-            // Crear el registro de servicios estándar
-            StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
-                    .configure("hibernate.cfg.xml")
-                    .build();
-            // Crear la metadata de Hibernate
-            Metadata metadata = new MetadataSources(standardRegistry)
-                    .getMetadataBuilder()
-                    .build();
-            // Crear la factoría de sesiones
-            sessionFactory = metadata.getSessionFactoryBuilder().build();
-        } catch (Throwable ex) {
-            // Capturar cualquier excepción y mostrarla
-            System.err.println("Error al inicializar la factoría de sesiones: " + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
+    }
+    public static boolean insertarUsuario(Usuarios usuario) {
+        Session sesion=sf.openSession();
+        sesion.beginTransaction();
+       try{
+        sesion.save(usuario);
+        sesion.getTransaction().commit();
+        return true;}
+       catch (Exception e) {
+           if (sesion.getTransaction() != null) {
+               sesion.getTransaction().rollback();
+           }
+           e.printStackTrace();
+           return false;
+       }
     }
 
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
 }
